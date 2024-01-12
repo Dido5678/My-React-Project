37b15fb123ee2198fd0d22784a39b4b61e472026
AidaMkrtchyan <aida.mkrtchyan0606@gmail.com>
2024-01-12 19:27:24 +0400

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,9 @@
 import { Container, Row, Col } from "react-bootstrap";
 import { MailchimpForm } from "./MailchimpForm";
-import logo from "../assets/img/logo.svg";
+import logo from "../assets/img/king-rem.png";
 import navIcon1 from "../assets/img/nav-icon1.svg";
 import navIcon2 from "../assets/img/nav-icon2.svg";
-import navIcon3 from "../assets/img/nav-icon3.svg";
+import navIcon3 from "../assets/img/icons8-github-128.svg";
 
 export const Footer = () => {
     return (
@@ -16,9 +16,9 @@ export const Footer = () => {
                         </Col>
                         <Col size={12} sm={6} className="text-center text-sm-end">
                             <div className="social-icon">
-                                <a href="#"><img src={navIcon1} alt="Icon" /></a>
-                                <a href="#"><img src={navIcon2} alt="Icon" /></a>
-                                <a href="#"><img src={navIcon3} alt="Icon" /></a>
+                                <a href="https://www.linkedin.com/in/aida-mkrtchyan-9b1a5324b/"><img src={navIcon1} alt="Icon" /></a>
+                                <a href="https://www.facebook.com/aida.mkrtchyan2"><img src={navIcon2} alt="Icon" /></a>
+                                <a href="https://github.com/Dido5678"><img src={navIcon3} alt="Icon" /></a>
                             </div>
                             <p>Copyright 2022. All Rights Reserved</p>
                         </Col>
